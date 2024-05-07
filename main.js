c196ec9c4f048b07f8d3c55ccfa87a3b024afc0a
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2024-05-07 06:27:03 +0200

    Update materia: MeshNormalMaterial

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const scene = new THREE.Scene()
 
 // Creo un cubo verde
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+//const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const material = new THREE.MeshNormalMaterial()
 
 // Creo la mesh
 const mesh = new THREE.Mesh(geometry, material)
